53c4b0040d5a1d47835cd0843ced52b9da3f1c92
Meet Kalathiya <meetkalathiya012@gmail.com>
2024-02-06 14:24:08 +0530

    filter-checkbox updated

--- src/components/FilterModal.jsx
+++ src/components/FilterModal.jsx
@@ -4,10 +4,8 @@ import { useDispatch } from "react-redux";
 import { setFilterData } from "../slices/filterSlice";
 import { IoClose } from "react-icons/io5";
 
-
 export default function FilterModal({ modalData }) {
-
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const [filters, setFilters] = useState({
     propertyType: [],
@@ -19,13 +17,6 @@ export default function FilterModal({ modalData }) {
     priceMax: "",
   });
 
-  const Checkbox = ({ label, checked, onChange }) => (
-    <label className="filter-checkbox">
-      <input type="checkbox" checked={checked} onChange={onChange} />
-      <p>{label}</p>
-    </label>
-  );
-
   const handleFilterChange = (field, value) => {
     setFilters((prevFilters) => {
       return { ...prevFilters, [field]: value };
@@ -35,7 +26,7 @@ export default function FilterModal({ modalData }) {
   const clearFilterHandler = () => {
     dispatch(setFilterData(null));
     modalData.cancelBtnHandler();
-  }
+  };
 
   const handlePropertyTypeChange = (value) => {
     setFilters((prevFilters) => {
@@ -56,122 +47,160 @@ export default function FilterModal({ modalData }) {
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    if( parseInt(filters.priceMin) > parseInt(filters.priceMax)){
-        toast.error("Min price should not greater than Max price")
+    if (parseInt(filters.priceMin) > parseInt(filters.priceMax)) {
+      toast.error("Min price should not greater than Max price");
+    } else {
+      dispatch(setFilterData(filters));
+      modalData.cancelBtnHandler();
     }
-    else{
-    dispatch(setFilterData(filters));
-    modalData.cancelBtnHandler();
-    }
-  }
+  };
 
   return (
     <div className="filter-wrapper">
-      <div className="filter" >
+      <div className="filter">
         <div className="filter-heading">
-          <h2>Filters <IoClose onClick={() => modalData.cancelBtnHandler()} className="filter-close-btn"/></h2>
-          <hr/>
-        </div>
-      
-      
-      <form className="filter-form" onSubmit={handleOnSubmit}>
-        <div className="filter-form-top">
-          Property Type
-          <Checkbox
-            label="Flat"
-            checked={filters.propertyType?.includes("Flat")}
-            onChange={() => handlePropertyTypeChange("Flat")}
-          />
-          <Checkbox
-            label="Bunglow"
-            checked={filters.propertyType?.includes("Bunglow")}
-            onChange={() => handlePropertyTypeChange("Bunglow")}
-          />
-          <Checkbox
-            label="Farmhouse"
-            checked={filters.propertyType?.includes("Farmhouse")}
-            onChange={() => handlePropertyTypeChange("Farmhouse")}
-          />
-          <Checkbox
-            label="Villa"
-            checked={filters.propertyType?.includes("Villa")}
-            onChange={() => handlePropertyTypeChange("Villa")}
-          />
-          <Checkbox
-            label="Land"
-            checked={filters.propertyType?.includes("Land")}
-            onChange={() => handlePropertyTypeChange("Land")}
-          />
-        </div>
-        <hr />
-        <div className="filter-form-bottom">
-        <label>
-          Price
-          <div className="filter-price">
-            <input
-              type="number"
-              placeholder="Min"
-              value={filters.priceMin}
-              onChange={(e) => handleFilterChange("priceMin", e.target.value)}
+          <h2>
+            Filters{" "}
+            <IoClose
+              onClick={() => modalData.cancelBtnHandler()}
+              className="filter-close-btn"
             />
-            <input
-              type="number"
-              placeholder="Max"
-              value={filters.priceMax}
-              onChange={(e) => handleFilterChange("priceMax", e.target.value)}
-            />
+          </h2>
+          <hr />
+        </div>
+
+        <form className="filter-form" onSubmit={handleOnSubmit}>
+          <div className="filter-form-top">
+            Property Type
+            <div id="checklist">
+              <input
+                defaultValue={1}
+                name="r"
+                checked={filters.propertyType?.includes("Flat")}
+                onChange={() => handlePropertyTypeChange("Flat")}
+                type="checkbox"
+                id={0o1}
+              />
+              <label htmlFor={0o1}>Flat</label>
+              <input
+                defaultValue={2}
+                name="r"
+                type="checkbox"
+                id={0o2}
+                checked={filters.propertyType?.includes("Bunglow")}
+                onChange={() => handlePropertyTypeChange("Bunglow")}
+              />
+              <label htmlFor={0o2}>Bunglow</label>
+              <input
+                defaultValue={3}
+                name="r"
+                type="checkbox"
+                id={0o3}
+                checked={filters.propertyType?.includes("Farmhouse")}
+                onChange={() => handlePropertyTypeChange("Farmhouse")}
+              />
+              <label htmlFor={0o3}>Farmhouse</label>
+              <input
+                defaultValue={3}
+                name="r"
+                type="checkbox"
+                id={0o4}
+                checked={filters.propertyType?.includes("Villa")}
+                onChange={() => handlePropertyTypeChange("Villa")}
+              />
+              <label htmlFor={0o4}>Villa</label>
+              <input
+                defaultValue={3}
+                name="r"
+                type="checkbox"
+                id={0o5}
+                checked={filters.propertyType?.includes("Land")}
+                onChange={() => handlePropertyTypeChange("Land")}
+              />
+              <label htmlFor={0o5}>Land</label>
+            </div>
           </div>
-        </label>
-        <hr />
-        <label>
-          Bedrooms
-          <input
-            type="number"
-            placeholder="no. of rooms"
-            value={filters.bhk}
-            onChange={(e) => handleFilterChange("bhk", e.target.value)}
-          />
-        </label>
-        <hr />
-        <label>
-          Bathrooms
-          <input
-            type="number"
-            placeholder="no. of bathrooms"
-            value={filters.bathrooms}
-            onChange={(e) => handleFilterChange("bathrooms", e.target.value)}
-          />
-        </label>
-        <hr />
-        <label>
-          City
-          <input
-            type="text"
-            placeholder="city"
-            value={filters.city}
-            onChange={(e) => handleFilterChange("city", e.target.value)}
-          />
-        </label>
-        <hr />
-        <label>
-          State
-          <input
-            type="text"
-            placeholder="state"
-            value={filters.state}
-            onChange={(e) => handleFilterChange("state", e.target.value)}
-          />
-        </label>
-        <hr />
-        <div className="filter-button">
-            <button className="clear-filter-btn" type="button" onClick={clearFilterHandler}>Clear filters</button>
-            <button className="search-filter-btn" type="submit">Search</button>
-        </div>
-        </div>
-      </form>
-      
+          <hr />
+          <div className="filter-form-bottom">
+            <label>
+              Price
+              <div className="filter-price">
+                <input
+                  type="number"
+                  placeholder="Min"
+                  value={filters.priceMin}
+                  onChange={(e) =>
+                    handleFilterChange("priceMin", e.target.value)
+                  }
+                />
+                <input
+                  type="number"
+                  placeholder="Max"
+                  value={filters.priceMax}
+                  onChange={(e) =>
+                    handleFilterChange("priceMax", e.target.value)
+                  }
+                />
+              </div>
+            </label>
+            <hr />
+            <label>
+              Bedrooms
+              <input
+                type="number"
+                placeholder="no. of rooms"
+                value={filters.bhk}
+                onChange={(e) => handleFilterChange("bhk", e.target.value)}
+              />
+            </label>
+            <hr />
+            <label>
+              Bathrooms
+              <input
+                type="number"
+                placeholder="no. of bathrooms"
+                value={filters.bathrooms}
+                onChange={(e) =>
+                  handleFilterChange("bathrooms", e.target.value)
+                }
+              />
+            </label>
+            <hr />
+            <label>
+              City
+              <input
+                type="text"
+                placeholder="city"
+                value={filters.city}
+                onChange={(e) => handleFilterChange("city", e.target.value)}
+              />
+            </label>
+            <hr />
+            <label>
+              State
+              <input
+                type="text"
+                placeholder="state"
+                value={filters.state}
+                onChange={(e) => handleFilterChange("state", e.target.value)}
+              />
+            </label>
+            <hr />
+            <div className="filter-button">
+              <button
+                className="clear-filter-btn"
+                type="button"
+                onClick={clearFilterHandler}
+              >
+                Clear filters
+              </button>
+              <button className="search-filter-btn" type="submit">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-      
     </div>
   );
 }
