17892a6afa9943f6ef1567e9ee83604684309883
Meet Kalathiya <meetkalathiya012@gmail.com>
2024-02-17 21:17:42 +0530

    not-subscribed status added in my profile

--- src/components/Myprofile.jsx
+++ src/components/Myprofile.jsx
@@ -2,20 +2,34 @@ import React from "react";
 import { FiPhone } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import { LuMail } from "react-icons/lu";
+import { Link } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Myprofile = () => {
   const { user } = useSelector((state) => state.profile);
 
-  const isSubscribed = new Date (user.subscriptionExpires).getTime() > new Date(Date.now()).getTime();
+  const isSubscribed =
+    new Date(user.subscriptionExpires).getTime() >
+    new Date(Date.now()).getTime();
 
   return (
     <div className="my-profile-wrapper">
       <h1>My profile</h1>
       <div className="my-profile-details">
         <div className="my-profile-main">
-          {
-            isSubscribed ? (<div className="subscribed">Subscribed</div>) : (<div className="not-subscribed">Not Subscribed</div>)
-          }
+          {isSubscribed ? (
+            <div className="subscribed">Subscribed</div>
+          ) : (
+            <div className="not-subscribed">
+              You are Not Subscribed yet !!
+              <Link
+                className="my-profile-plan"
+                to={"/plan"}
+              >
+                Please get a plan <FaArrowRightLong />
+              </Link>
+            </div>
+          )}
           <img src={user.image} alt="" />
           <div className="my-profile-main-details">
             <h2>{user?.accountType}</h2>
@@ -24,11 +38,11 @@ const Myprofile = () => {
             </h3>
             {user.additionalDetails.contactNumber && (
               <div>
-              <FiPhone />
-              {user?.additionalDetails?.contactNumber}
-            </div>
+                <FiPhone />
+                {user?.additionalDetails?.contactNumber}
+              </div>
             )}
-            
+
             <div>
               <LuMail />
               {user?.email}
@@ -37,7 +51,7 @@ const Myprofile = () => {
           </div>
         </div>
         <div className="my-profile-additional-details">
-            <h2>Personal Details</h2>
+          <h2>Personal Details</h2>
           <div className="my-profile-additional-details-table">
             <div className="my-profile-additional-details-left">
               <div>First Name</div>
@@ -45,15 +59,27 @@ const Myprofile = () => {
               <div>Email</div>
               <p>{user?.email}</p>
               <div>Gender</div>
-              <p>{user?.additionalDetails?.gender ? (user?.additionalDetails?.gender) : ("-")}</p>
+              <p>
+                {user?.additionalDetails?.gender
+                  ? user?.additionalDetails?.gender
+                  : "-"}
+              </p>
             </div>
             <div className="my-profile-additional-details-left">
               <div>Last Name</div>
               <p>{user?.lastName}</p>
               <div>Phone Number</div>
-              <p>{user?.additionalDetails?.contactNumber ? (user?.additionalDetails?.contactNumber) : ("-")}</p>
+              <p>
+                {user?.additionalDetails?.contactNumber
+                  ? user?.additionalDetails?.contactNumber
+                  : "-"}
+              </p>
               <div>Date of Birth</div>
-              <p>{user?.additionalDetails?.dateOfBirth ? (user?.additionalDetails?.dateOfBirth) : ("-")}</p>
+              <p>
+                {user?.additionalDetails?.dateOfBirth
+                  ? user?.additionalDetails?.dateOfBirth
+                  : "-"}
+              </p>
             </div>
           </div>
         </div>
