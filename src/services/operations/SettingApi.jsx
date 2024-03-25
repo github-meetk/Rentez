41430e71ca7ab843ec14815ceba4f44710bb50dd
Meet Kalathiya <meetkalathiya012@gmail.com>
2024-03-25 11:40:21 +0530

    Delete account api handled on client side effectively

--- src/services/operations/SettingApi.jsx
+++ src/services/operations/SettingApi.jsx
@@ -1,16 +1,17 @@
-import { toast } from "react-hot-toast"
+import { toast } from "react-hot-toast";
 
-import { setUser } from "../../slices/profileSlice"
-import { apiConnector } from "../apiconnector"
-import { settingsEndpoints } from "../apis"
-import { logout } from "./authAPI"
+import { setUser } from "../../slices/profileSlice";
+import { apiConnector } from "../apiconnector";
+import { settingsEndpoints } from "../apis";
+import { setToken } from "../../slices/authSlice";
+import { resetList } from "../../slices/cartSlice";
 
 const {
   UPDATE_DISPLAY_PICTURE_API,
   UPDATE_PROFILE_API,
   CHANGE_PASSWORD_API,
   DELETE_PROFILE_API,
-} = settingsEndpoints
+} = settingsEndpoints;
 
 export function updateDisplayPicture(token, formData) {
   return async (dispatch) => {
@@ -24,23 +25,23 @@ export function updateDisplayPicture(token, formData) {
           "Content-Type": "multipart/form-data",
           Authorization: `Bearer ${token}`,
         }
-      )
+      );
       console.log(
         "UPDATE_DISPLAY_PICTURE_API API RESPONSE............",
         response
-      )
+      );
 
       if (!response.data.success) {
-        throw new Error(response.data.message)
+        throw new Error(response.data.message);
       }
-      toast.success("Display Picture Updated Successfully")
-      dispatch(setUser(response.data.data))
+      toast.success("Display Picture Updated Successfully");
+      dispatch(setUser(response.data.data));
     } catch (error) {
-      console.log("UPDATE_DISPLAY_PICTURE_API API ERROR............", error)
-      toast.error(error.response?.data.message)
+      console.log("UPDATE_DISPLAY_PICTURE_API API ERROR............", error);
+      toast.error(error.response?.data.message);
     }
     // toast.dismiss(toastId)
-  }
+  };
 }
 
 export function updateProfile(token, formData) {
@@ -49,45 +50,44 @@ export function updateProfile(token, formData) {
     try {
       const response = await apiConnector("PUT", UPDATE_PROFILE_API, formData, {
         Authorization: `Bearer ${token}`,
-      })
-      console.log("UPDATE_PROFILE_API API RESPONSE............", response)
+      });
+      console.log("UPDATE_PROFILE_API API RESPONSE............", response);
 
       if (!response.data.success) {
-        throw new Error(response.data.message)
+        throw new Error(response.data.message);
       }
       const userImage = response.data.updatedUserDetails.image
         ? response.data.updatedUserDetails.image
-        : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.updatedUserDetails.firstName} ${response.data.updatedUserDetails.lastName}`
+        : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.updatedUserDetails.firstName} ${response.data.updatedUserDetails.lastName}`;
       dispatch(
         setUser({ ...response.data.updatedUserDetails, image: userImage })
-      )
-      toast.success("Profile Updated Successfully")
+      );
+      toast.success("Profile Updated Successfully");
     } catch (error) {
-      console.log("UPDATE_PROFILE_API API ERROR............", error)
-      toast.error("Could Not Update Profile")
+      console.log("UPDATE_PROFILE_API API ERROR............", error);
+      toast.error("Could Not Update Profile");
     }
     // toast.dismiss(toastId)
-  }
+  };
 }
 
 export async function changePassword(token, formData) {
-  const toastId = toast.loading("Loading...")
+  const toastId = toast.loading("Loading...");
   try {
     const response = await apiConnector("POST", CHANGE_PASSWORD_API, formData, {
       Authorization: `Bearer ${token}`,
-    })
-    console.log("CHANGE_PASSWORD_API API RESPONSE............", response)
+    });
+    console.log("CHANGE_PASSWORD_API API RESPONSE............", response);
     if (!response.data.success) {
-      throw new Error(response.data.message)
+      throw new Error(response.data.message);
     }
-    
-    toast.success("Password Changed Successfully")
+
+    toast.success("Password Changed Successfully");
   } catch (error) {
-    console.log("CHANGE_PASSWORD_API API ERROR............", error)
-    toast.error(error.response.data.message)
+    console.log("CHANGE_PASSWORD_API API ERROR............", error);
+    toast.error(error.response.data.message);
   }
-  toast.dismiss(toastId)
-  
+  toast.dismiss(toastId);
 }
 
 export function deleteProfile(token, navigate) {
@@ -96,18 +96,25 @@ export function deleteProfile(token, navigate) {
     try {
       const response = await apiConnector("DELETE", DELETE_PROFILE_API, null, {
         Authorization: `Bearer ${token}`,
-      })
-      console.log("DELETE_PROFILE_API API RESPONSE............", response)
+      });
+      console.log("DELETE_PROFILE_API API RESPONSE............", response);
 
       if (!response.data.success) {
-        throw new Error(response.data.message)
+        throw new Error(response.data.message);
       }
-      toast.success("Profile Deleted Successfully")
-      dispatch(logout(navigate))
+      toast.success("Profile Deleted Successfully");
+      dispatch(setToken(null));
+      dispatch(setUser(null));
+      dispatch(resetList());
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      localStorage.removeItem("cart");
+      localStorage.removeItem("totalItems");
+      navigate("/");
     } catch (error) {
-      console.log("DELETE_PROFILE_API API ERROR............", error)
-      toast.error("Could Not Delete Profile")
+      console.log("DELETE_PROFILE_API API ERROR............", error);
+      toast.error("Could Not Delete Profile");
     }
     // toast.dismiss(toastId)
-  }
+  };
 }
