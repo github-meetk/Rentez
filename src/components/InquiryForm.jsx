1703fefdb8278731315157dc87f436a0e13def62
Meet Kalathiya <meetkalathiya012@gmail.com>
2024-03-26 14:55:30 +0530

    scroll top set on property detail page

---
+++ src/components/InquiryForm.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { notifySeller } from "../services/operations/propertyAPI";
+
+const InquiryForm = ({
+  token,
+  setReviewModal,
+  setConfirmationModal,
+  sellerEmail,
+}) => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    fullName: "",
+    contactNumber: "",
+    msg: "",
+  });
+
+  const { email, fullName, contactNumber, msg } = formData;
+
+  const handleOnChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    if (!token) {
+      setConfirmationModal({
+        text1: "You are not logged in!",
+        text2: "Please login to Purchase Course.",
+        btn1Text: "Login",
+        btn2Text: "Cancel",
+        btn1Handler: () => navigate("/login"),
+        btn2Handler: () => setConfirmationModal(null),
+      });
+    } else {
+      await notifySeller(sellerEmail, email, fullName, contactNumber, msg);
+      setFormData({
+        email: "",
+        fullName: "",
+        contactNumber: "",
+        msg: "",
+      });
+
+      setReviewModal({
+        cancelBtnHandler: () => setReviewModal(null),
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <h1>Request Inquiry</h1>
+      <input
+        required
+        type="text"
+        name="fullName"
+        value={fullName}
+        onChange={handleOnChange}
+        placeholder="Full Name"
+      />
+      <input
+        required
+        type="text"
+        name="email"
+        value={email}
+        onChange={handleOnChange}
+        placeholder="Email"
+      />
+      <input
+        required
+        type="number"
+        name="contactNumber"
+        value={contactNumber}
+        onChange={handleOnChange}
+        placeholder="Phone Number"
+      />
+      <input
+        required
+        type="text"
+        name="msg"
+        value={msg}
+        onChange={handleOnChange}
+        placeholder="Message"
+      />
+      <button type="submit" className="send-btn">
+        <div className="svg-wrapper-1">
+          <div className="svg-wrapper">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width={24}
+              height={24}
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path
+                fill="currentColor"
+                d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+              />
+            </svg>
+          </div>
+        </div>
+        <span>Send Request</span>
+      </button>
+    </form>
+  );
+};
+
+export default InquiryForm;
--- src/pages/DetailProperty.jsx
+++ src/pages/DetailProperty.jsx
@@ -1,69 +1,18 @@
 import React, { useEffect, useState } from "react";
 import NavbarLight from "../components/NavbarLight";
 import Footer from "../components/Footer";
-import {
-  getPropertyDetail,
-  notifySeller,
-} from "../services/operations/propertyAPI";
-import { useNavigate, useParams } from "react-router-dom";
+import { getPropertyDetail } from "../services/operations/propertyAPI";
+import { useParams } from "react-router-dom";
 import ReviewModal from "../components/ReviewModal";
 import ConfirmationModal from "../components/ConfirmationModal";
 import { useSelector } from "react-redux";
+import InquiryForm from "../components/InquiryForm";
 
 const DetailProperty = () => {
-  const navigate = useNavigate();
-  const {token} = useSelector((state) => state.auth);
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+  const { token } = useSelector((state) => state.auth);
   const [reviewModal, setReviewModal] = useState(null);
-  const [confirmationModal, setConfirmationModal] = useState(null)
-
-  const [formData, setFormData] = useState({
-    email: "",
-    fullName: "",
-    contactNumber: "",
-    msg: "",
-  });
-
-  const { email, fullName, contactNumber, msg } = formData;
-
-  const handleOnChange = (e) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleOnSubmit = async (e) => {
-    e.preventDefault();
-    if(!token){
-      setConfirmationModal({
-        text1: "You are not logged in!",
-        text2: "Please login to Purchase Course.",
-        btn1Text: "Login",
-        btn2Text: "Cancel",
-        btn1Handler: () => navigate("/login"),
-        btn2Handler: () => setConfirmationModal(null),
-      })
-    }else{
-    await notifySeller(
-      detail?.seller?.email,
-      email,
-      fullName,
-      contactNumber,
-      msg
-    );
-    // navigate("/");
-    setFormData({
-      email: "",
-      fullName: "",
-      contactNumber: "",
-      msg: "",
-    });
-
-    setReviewModal({
-      cancelBtnHandler: () => setReviewModal(null),
-    });
-  }
-  };
+  const [confirmationModal, setConfirmationModal] = useState(null);
 
   const { propertyId } = useParams();
   const [images, setImages] = useState([]);
@@ -127,16 +76,27 @@ const DetailProperty = () => {
                   </h1>{" "}
                   <p>Rent</p>
                 </div>
-                <h3>Total Rooms : <span>{detail?.bhk}</span></h3>
-                <h3>Total Bathrooms : <span>{detail?.bathrooms}</span></h3>
                 <h3>
-                  Listed at : <span>{new Date(detail?.createdAt).toDateString()}</span>
+                  Total Rooms : <span>{detail?.bhk}</span>
+                </h3>
+                <h3>
+                  Total Bathrooms : <span>{detail?.bathrooms}</span>
+                </h3>
+                <h3>
+                  Listed at :{" "}
+                  <span>{new Date(detail?.createdAt).toDateString()}</span>
                 </h3>
                 <p>
-                  Address : <span>{detail?.address} {detail?.city} {detail?.state}{" "}
-                  {detail?.pincode}</span>
+                  Address :{" "}
+                  <span>
+                    {detail?.address} {detail?.city} {detail?.state}{" "}
+                    {detail?.pincode}
+                  </span>
                 </p>
-                <p> Description : <span>{detail?.description}</span></p>
+                <p>
+                  {" "}
+                  Description : <span>{detail?.description}</span>
+                </p>
               </div>
 
               <div className="seller-details">
@@ -156,60 +116,12 @@ const DetailProperty = () => {
               </div>
             </div>
             <div className="property-details-section-right">
-              <form onSubmit={handleOnSubmit}>
-                <h1>Request Inquiry</h1>
-                <input
-                  required
-                  type="text"
-                  name="fullName"
-                  value={fullName}
-                  onChange={handleOnChange}
-                  placeholder="Full Name"
-                />
-                <input
-                  required
-                  type="text"
-                  name="email"
-                  value={email}
-                  onChange={handleOnChange}
-                  placeholder="Email"
-                />
-                <input
-                  required
-                  type="number"
-                  name="contactNumber"
-                  value={contactNumber}
-                  onChange={handleOnChange}
-                  placeholder="Phone Number"
-                />
-                <input
-                  required
-                  type="text"
-                  name="msg"
-                  value={msg}
-                  onChange={handleOnChange}
-                  placeholder="Message"
-                />
-                <button type="submit" className="send-btn">
-                  <div className="svg-wrapper-1">
-                    <div className="svg-wrapper">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width={24}
-                        height={24}
-                      >
-                        <path fill="none" d="M0 0h24v24H0z" />
-                        <path
-                          fill="currentColor"
-                          d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <span>Send Request</span>
-                </button>
-              </form>
+              <InquiryForm
+                token={token}
+                setReviewModal={setReviewModal}
+                setConfirmationModal={setConfirmationModal}
+                sellerEmail={detail?.seller?.email}
+              />
             </div>
           </div>
         </div>
