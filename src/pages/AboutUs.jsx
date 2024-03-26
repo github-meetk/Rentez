450588af2b8d013e23717c9b7384014b66b77e04
Meet Kalathiya <meetkalathiya012@gmail.com>
2024-03-26 15:05:03 +0530

    scroll top added on about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,14 +3,14 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import aboutHero from "../assets/community-people.webp";
 import aboutVision from "../assets/about-hero.webp";
-import benefit from "../assets/benefit.avif"
+import benefit from "../assets/benefit.avif";
 import toast from "react-hot-toast";
 import { apiConnector } from "../services/apiconnector";
 import { contactusEndpoint } from "../services/apis";
 import Subscibe from "../components/Subscibe";
 
 const AboutUs = () => {
-  // document.body.scrollTop = document.documentElement.scrollTop = 0;
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
 
   const [formData, setFormData] = useState({
     firstName: "",
@@ -29,7 +29,7 @@ const AboutUs = () => {
     }));
   };
 
-  const handleOnSubmit = async(e) => {
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
     const toastId = toast.loading("Loading...");
     try {
@@ -37,18 +37,19 @@ const AboutUs = () => {
         "POST",
         contactusEndpoint.CONTACT_US_API,
         formData
-      )
+      );
 
       if (!response.data.success) {
         toast.dismiss(toastId);
-        return
+        return;
       }
 
       toast.success("Message Sent Successfully");
-
     } catch (error) {
-      console.log("ERROR MESSAGE - ", error.message)
-      error.response.data.message ? toast.error(error.response.data.message) : toast.error(error)
+      console.log("ERROR MESSAGE - ", error.message);
+      error.response.data.message
+        ? toast.error(error.response.data.message)
+        : toast.error(error);
     }
     toast.dismiss(toastId);
 
@@ -66,52 +67,60 @@ const AboutUs = () => {
     <div>
       <Navbar />
       <div className="about-wrapper">
-      <div className="about-hero-wrapper">
-        <h1>Welcome to Rentez</h1>
-        <p>
-          At Rentez, we seamlessly connect property owners with tenants,
-          bridging the gap between listings and those in search of the perfect
-          rental.
-        </p>
-        <img src={aboutHero} alt=""></img>
-      </div>
-      <div className="about-vision-wrapper">
-        <h1>Our Vision</h1>
-        <img src={aboutVision} alt="" />
-        <p>
-          In our vision, we see a world where finding the perfect property or
-          listing your space becomes a seamless and enjoyable experience.
-          <br />
-          <br /> Rentez is not just a platform; it's a commitment to redefine
-          how you rent, transforming the process into a journey of meaningful
-          connections.
-          <br /> <br />
-          We go beyond traditional transactions, aiming to make renting more
-          than just a transaction but a meaningful and enriching connection with
-          the spaces that matter most to you. Welcome to Rentez, where we
-          redefine renting with a focus on seamless experiences and meaningful
-          connections.
-        </p>
-      </div>
-      <Subscibe/>
-      <div className="about-benefit-wrapper">
-        <div className="about-benefit-left">
-          <h1>What are the benefits of being at Rentez?</h1>
-          <ul>
-            <li>Simplified, efficient rental process from listing to finding the perfect space.</li>
-            <li>Diverse property options for apartments, houses, and commercial spaces.</li>
-            <li>User-friendly interface for smooth navigation.</li>
-            <li>Transparency and trust in every aspect of the rental process.</li>
-            <li>Property owners enjoy an efficient listing process.</li>
-          </ul>
+        <div className="about-hero-wrapper">
+          <h1>Welcome to Rentez</h1>
+          <p>
+            At Rentez, we seamlessly connect property owners with tenants,
+            bridging the gap between listings and those in search of the perfect
+            rental.
+          </p>
+          <img src={aboutHero} alt=""></img>
         </div>
-          <img className="about-benefit-right" src={benefit} alt=""/>
-      </div>
-      <div className="contact-form-wrapper">
-        <form className="contact-form" onSubmit={handleOnSubmit}>
-          <h1>Get In Touch</h1>
-          <p>We'd love to here for you, Please fill out this form.</p>
-          <input
+        <div className="about-vision-wrapper">
+          <h1>Our Vision</h1>
+          <img src={aboutVision} alt="" />
+          <p>
+            In our vision, we see a world where finding the perfect property or
+            listing your space becomes a seamless and enjoyable experience.
+            <br />
+            <br /> Rentez is not just a platform; it's a commitment to redefine
+            how you rent, transforming the process into a journey of meaningful
+            connections.
+            <br /> <br />
+            We go beyond traditional transactions, aiming to make renting more
+            than just a transaction but a meaningful and enriching connection
+            with the spaces that matter most to you. Welcome to Rentez, where we
+            redefine renting with a focus on seamless experiences and meaningful
+            connections.
+          </p>
+        </div>
+        <Subscibe />
+        <div className="about-benefit-wrapper">
+          <div className="about-benefit-left">
+            <h1>What are the benefits of being at Rentez?</h1>
+            <ul>
+              <li>
+                Simplified, efficient rental process from listing to finding the
+                perfect space.
+              </li>
+              <li>
+                Diverse property options for apartments, houses, and commercial
+                spaces.
+              </li>
+              <li>User-friendly interface for smooth navigation.</li>
+              <li>
+                Transparency and trust in every aspect of the rental process.
+              </li>
+              <li>Property owners enjoy an efficient listing process.</li>
+            </ul>
+          </div>
+          <img className="about-benefit-right" src={benefit} alt="" />
+        </div>
+        <div className="contact-form-wrapper">
+          <form className="contact-form" onSubmit={handleOnSubmit}>
+            <h1>Get In Touch</h1>
+            <p>We'd love to here for you, Please fill out this form.</p>
+            <input
               required
               type="text"
               name="firstName"
@@ -136,8 +145,7 @@ const AboutUs = () => {
               value={email}
               onChange={handleOnChange}
               placeholder="Email"
-            >
-            </input>
+            ></input>
             <input
               required
               type="number"
@@ -145,17 +153,18 @@ const AboutUs = () => {
               value={phoneNo}
               onChange={handleOnChange}
               placeholder="Mobile no."
-            >
-            </input>
+            ></input>
             <textarea
-                name="message"
-                value={message}
-                onChange={handleOnChange}
-                placeholder="Enter your message here..."
-              />
-            <button className="special-btn" type="submit">Send Message</button>
-        </form>
-      </div>
+              name="message"
+              value={message}
+              onChange={handleOnChange}
+              placeholder="Enter your message here..."
+            />
+            <button className="special-btn" type="submit">
+              Send Message
+            </button>
+          </form>
+        </div>
       </div>
       <Footer />
     </div>
