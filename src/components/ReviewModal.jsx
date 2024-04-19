cccba282d081b348a3bf81dd9d435e5f4032f58e
Meet Kalathiya <meetkalathiya012@gmail.com>
2024-04-19 22:59:44 +0530

    review modal bug fixed

--- src/components/ReviewModal.jsx
+++ src/components/ReviewModal.jsx
@@ -22,7 +22,7 @@ const ReviewModal = ({ modalData }) => {
     }));
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     if (!token) {
       toast.error("Please Login first");
       return;
@@ -45,63 +45,65 @@ const ReviewModal = ({ modalData }) => {
     <div className="review-modal-wrapper">
       <div className="review-box">
         <h2>Rate and Review</h2>
-        <div className="rating-container">
-          Rating {hover || rating} out of 5
-          <div className="rating">
-            {[...Array(5)].map((star, i) => {
-              const ratingValue = i + 1;
+        <form
+          onSubmit={() => {
+            handleSubmit();
+            modalData.cancelBtnHandler();
+          }}
+        >
+          <div className="rating-container">
+            Rating {hover || rating} out of 5
+            <div className="rating">
+              {[...Array(5)].map((star, i) => {
+                const ratingValue = i + 1;
 
-              return (
-                <label key={i}>
-                  <input
-                    style={{ display: "none" }}
-                    required
-                    type="radio"
-                    name="rating"
-                    value={ratingValue}
-                    onClick={() => setRating(ratingValue)}
-                  />
-                  <FaStar
-                    className="star"
-                    color={
-                      ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"
-                    }
-                    size={55}
-                    onMouseEnter={() => setHover(ratingValue)}
-                    onMouseLeave={() => setHover(null)}
-                  />
-                </label>
-              );
-            })}
+                return (
+                  <label key={i}>
+                    <input
+                      style={{ display: "none" }}
+                      required
+                      type="radio"
+                      name="rating"
+                      value={ratingValue}
+                      onClick={() => setRating(ratingValue)}
+                    />
+                    <FaStar
+                      className="star"
+                      color={
+                        ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"
+                      }
+                      size={55}
+                      onMouseEnter={() => setHover(ratingValue)}
+                      onMouseLeave={() => setHover(null)}
+                    />
+                  </label>
+                );
+              })}
+            </div>
           </div>
-        </div>
-        <label className="review">
-          Review
-          <textarea
-            required
-            value={review}
-            name="review"
-            onChange={handleChange}
-          />
-        </label>
+          <label className="review">
+            Review
+            <textarea
+              required
+              value={review}
+              name="review"
+              onChange={handleChange}
+            />
+          </label>
 
-        <div className="review-buttons">
-          <button
-            onClick={modalData.cancelBtnHandler}
-            className="clear-filter-btn"
-          >
-            Cancel
-          </button>
-          <button
-            onClick={() => {
-              handleSubmit();
-              modalData.cancelBtnHandler();
-            }}
-            className="modal-button-1"
-          >
-            Submit
-          </button>
-        </div>
+          <div className="review-buttons">
+            <button
+              type="button"
+              onClick={modalData.cancelBtnHandler}
+              className="clear-filter-btn"
+            >
+              Cancel
+            </button>
+            <button type="submit" className="modal-button-1">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
